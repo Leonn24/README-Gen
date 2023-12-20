95aa616ecd313b88d1892cafd7a6c7060d48e3e2
Leonn24 <82012128+Leonn24@users.noreply.github.com>
2023-12-20 06:35:54 -0500

    added JS, installed inquirer

---
+++ README.md
@@ -0,0 +1,34 @@
+# READMEGEN
+  
+  ## Description
+  creates a readme file
+
+  ## Table of Contents
+  - [Installation]{#installation}
+  - [Usage]{#usage}
+  - [License]{#license}
+  - [Contributing]{#contributing}
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Usage
+  node app.js
+
+  ## Contributing
+  undefined
+
+  ## License
+  [![License](https://img.shields.io/badge/license-Do%20What%20The%20F*ck%20You%20Want%20To%20Public%20License-green)](./LICENSE)
+  This project is licensed under the Do What The F*ck You Want To Public License
+
+  ## Contributing
+  undefined
+
+  ## Tests
+  undefined
+
+  ## Questions
+  undefined
+  For additional questions you can reach me throught:
+  - [GitHub](http://github.com/Leonn24)
+  - [Email] leonn.h24@gmail.com
---
+++ index.js
@@ -0,0 +1,99 @@
+import fs from 'fs';
+import inquirer from 'inquirer';
+
+async function promptUser(){
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your Project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'What does this application do?',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter installation instructions'
+    },
+    {
+      type:'input',
+      name: 'usage',
+      message:'Enter usage information'
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Select a licennse for this application',
+      choices: ['MIT', 'Academic Free License v3.0', 'Apache license 2.0', 'Artistic license 2.0', 'Do What The F*ck You Want To Public License', 'Boost Software License 1.0', 'Eclipse Public License 1.0', 'Microsoft Public License']
+    },
+    {
+      type: 'confirm',
+      name: 'contribution',
+      message: 'Will your project be open to contributions?'
+    },
+    {
+      type:'input',
+      name:'githubUsername',
+      message:'Enter your GitHub Username'
+    },
+    {
+      type:'input',
+      name:'email',
+      message:'Enter your email address'
+    },
+
+  ])
+}
+  
+function generateREADME(data) {
+  return `# ${data.title}
+  
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation]{#installation}
+  - [Usage]{#usage}
+  - [License]{#license}
+  - [Contributing]{#contributing}
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## License
+  [![License](https://img.shields.io/badge/license-${encodeURIComponent(data.license)}-green)](./LICENSE)
+  This project is licensed under the ${data.license}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  ${data.questions}
+  For additional questions you can reach me throught:
+  - [GitHub](http://github.com/${data.githubUsername})
+  - [Email] ${data.email}`;
+}
+
+async function init() {
+  try {
+    const userData = await promptUser();
+    const readmeInfo = generateREADME(userData);
+    fs.writeFileSync('README.md', readmeInfo);
+    console.log('README.md generated successfully!');
+  } catch (error) {
+    console.error('Error generating README', error.message);
+  }
+}
+
+init();
